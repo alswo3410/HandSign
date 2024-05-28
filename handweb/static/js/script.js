b5a1befd00525ce75a2317471844b21cf2a77bbb
al <52342012+alswo3410@users.noreply.github.com>
2024-05-28 11:26:38 +0900

    Delete handweb directory

--- handweb/static/js/script.js
+++
@@ -1,8 +0,0 @@
-function saveLetter() {
-    fetch('/save_letter', { method: 'POST' })
-        .then(response => response.json())
-        .then(data => {
-            const savedLettersDiv = document.getElementById('saved-letters');
-            savedLettersDiv.innerHTML = data.saved_letters.join(' ');
-        });
-}
